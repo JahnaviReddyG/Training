79157a67ed4b16fc44f67a8f000bbf133537ca85
jahnavireddy <40195658+JahnaviReddyG@users.noreply.github.com>
2018-08-27 15:12:25 +0530

    Add files via upload
    Javascript in browsers

---
+++ todo-app.js
@@ -0,0 +1,26 @@
+const todo = [{
+    text: 'order cast food',
+    completed: true
+},{
+    text:'Clean kitchen',
+    completed: false
+}]
+//const incompletTodos = todo.filter(function(todo){
+  //  return  !todo.completed
+//})
+//const newParagraph = document.createElement('p')
+//newParagraph.textContent = `you have ${incompletTodos.length} todos lenght`
+//document.querySelector('body').appendChild(newParagraph)
+
+/*const ps = document.querySelectorAll('p')
+ps.forEach(function(a)
+{
+    if(a.textContent.includes('the'))
+ {
+     a.remove()
+ }
+  
+})*/
+document.querySelector('button').addEventListener('click',function(e){
+console.log("heyyy")
+})
